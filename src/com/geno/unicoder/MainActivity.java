7994e6ce33c09b2e8486bdb65487337cc99111b1
Geno1024 <754097987@qq.com>
2015-06-11 09:02:22 +0800

    [init]change to a computer for further work

--- src/com/geno/unicoder/MainActivity.java
+++ src/com/geno/unicoder/MainActivity.java
@@ -4,14 +4,44 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.text.*;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
+	public EditText in, out;
+	@Override
+	public void onCreate(Bundle savedInstanceState)
 	{
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		in = (EditText) findViewById(R.id.in);
+		out = (EditText) findViewById(R.id.out);
+		in.setHint("Hex without 0x prefix here. For example 41.");
+		TextWatcher t = new TextWatcher()
+		{
+
+			@Override
+			public void beforeTextChanged(CharSequence p1, int p2, int p3, int p4)
+			{
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence p1, int p2, int p3, int p4)
+			{
+				String unicode = in.getText().toString();
+				
+				int cp = Integer.parseInt("0x" + unicode);
+				char output = Character.toChars(cp)[0];
+				out.setText(output);
+			}
+
+			@Override
+			public void afterTextChanged(Editable p1)
+			{
+				
+			}
+		};
+		in.addTextChangedListener(t);
+	}
 }
